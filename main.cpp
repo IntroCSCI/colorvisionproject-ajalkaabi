6fe39d4e1a1a4538630238056bef9f17c377b8a9
Ahmed Alkaabi <aalkaabi@mail.csuchico.edu>
2021-03-13 16:50:54 +0000

    main/read.me

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main() {
         
        
 				for (int colorValue = position+=1; (line[colorValue] >= '0' && line[colorValue] <= '9') || ((line[colorValue] >= 'A' && line[colorValue] <= 'F')) || (line[colorValue] >= 'a' && line[colorValue] <= 'f'); colorValue+=6) {
-
+        
           
           string i = line.substr(position, 6);
           if(i.size()==6)
